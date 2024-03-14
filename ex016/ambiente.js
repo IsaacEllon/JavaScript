693606c2d792eedeaa3740329278d74b89fe715f
Isaac Ellon <155269775+IsaacEllon@users.noreply.github.com>
2024-03-14 18:11:03 -0300

    2

--- ex016/ambiente.js
+++ ex016/ambiente.js
@@ -1,1 +1,18 @@
+//teste lógico é feito no final
+
 var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+
+//teste lógico é feito no início
+
+/*
+    var c = 1
+    while ( c <= 6) {
+        console.log(`passo ${c}`)
+        c++ // c = c + 1
+}
+*/
---
+++ ex016/ambiente1.js
@@ -0,0 +1,10 @@
+// estrutura de incremento com variável de controle
+// for (inicio; teste; incremento) { }
+
+console.log('Vai começar!')
+
+for (var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+
+console.log('FIM!')
